ac932de095286ef724f06e455603b276cdaaf6dd
niacobel <56349930+niacobel@users.noreply.github.com>
2020-10-13 13:58:16 +0200

    Manual merge of nico into master for problematic files

--- abin_launcher/benchmark.py
+++ abin_launcher/benchmark.py
@@ -35,15 +35,19 @@ def get_Elapsed(jobID:int) -> str:
 
 def get_MaxRSS(jobID:int) -> int:
   """ Maximum resident set size of all tasks in job. (Note: expressed in KB, converted in MB) """
-  maxRSS_k = str(subprocess.check_output("sacct -j {} --format=MaxRSS%12 --noheader | head -n2 | tr -d [:space:]".format(jobID), shell=True).decode('utf-8'))
-  maxRSS_k = maxRSS_k.strip()
-  maxRSS_m = "-1"
-  if (maxRSS_k is not None and maxRSS_k != ''):
-    # Cut the K at the end of MaxRSS
-    maxRSS_k = maxRSS_k.rstrip('k')
-    maxRSS_k = maxRSS_k.rstrip('K')
-    # Convert to MB
-    maxRSS_m = int(int(maxRSS_k) / 1024)
+  # Get all maxRSS values from all job steps
+  maxRSS_raw = str(subprocess.check_output("sacct -j {} --format=MaxRSS%12 --noheader".format(jobID), shell=True).decode('utf-8'))
+  maxRSS_lines = maxRSS_raw.splitlines()
+  # Get rid of whitespaces 
+  maxRSS_list = [line.strip() for line in maxRSS_lines if line.strip() != '']
+  # Check if there no unknown value
+  for line in maxRSS_list:
+    if line[-1].lower() != 'k' and line[-1].lower() != 'm':
+      raise ValueError ("One of the values returned by 'sacct -j {} --format=MaxRSS%12 --noheader' is of unknown units".format(jobID))
+  # Get rid of the k / K or m / M at the end of the numbers (and convert KB to MB while we're at it)
+  maxRSS_list_m = [(int(line.rstrip('k').rstrip('K')) / 1024) for line in maxRSS_list if line[-1].lower() == 'k'] + [float(line.rstrip('m').rstrip('M')) for line in maxRSS_list if line[-1].lower() == 'm']
+  # Get the highest maxRSS from that list
+  maxRSS_m = int(max(maxRSS_list_m))
   return maxRSS_m
 
 def get_ReqCPUs(jobID:int) -> int:
@@ -83,214 +87,216 @@ def slurm_time_to_seconds(time:str) -> int:
   seconds=sum(x * int(t) for x, t in zip([1, 60, 3600, 86400], reversed(time_tmp.split(":"))))
   return seconds
 
+# ===================================================================
+# ===================================================================
+# Command line arguments
+# ===================================================================
+# ===================================================================
 
-def main():
-  # ===================================================================
-  # ===================================================================
-  # Command line arguments
-  # ===================================================================
-  # ===================================================================
+# Define the arguments needed for the script (here they are defined as named arguments rather than positional arguments, check https://stackoverflow.com/questions/24180527/argparse-required-arguments-listed-under-optional-arguments for more info).
 
-  # Define the arguments needed for the script (here they are defined as named arguments rather than positional arguments, check https://stackoverflow.com/questions/24180527/argparse-required-arguments-listed-under-optional-arguments for more info).
+parser = argparse.ArgumentParser(add_help=False, description="")
 
-  parser = argparse.ArgumentParser(add_help=False, description="")
+required = parser.add_argument_group('Required arguments')
+required.add_argument("--tmp", type=str, help="CSV file name you want to enrich", required=True)
+required.add_argument("--final", type=str, help="Destination CSV file name", required=True)
 
-  required = parser.add_argument_group('Required arguments')
-  required.add_argument("--tmp", type=str, help="CSV file name you want to enrich", required=True)
-  required.add_argument("--final", type=str, help="Destination CSV file name", required=True)
+optional = parser.add_argument_group('Optional arguments')
+optional.add_argument('-h','--help',action='help',default=argparse.SUPPRESS,help='Show this help message and exit')
 
-  optional = parser.add_argument_group('Optional arguments')
-  optional.add_argument('-h','--help',action='help',default=argparse.SUPPRESS,help='Show this help message and exit')
-  optional.add_argument("-k","--keep",action="store_true",help="Do not rename the treated CSV file")
+args = parser.parse_args()
 
-  args = parser.parse_args()
+# Define the variables corresponding to those arguments
 
-  # Define the variables corresponding to those arguments
+csv_tmp = args.tmp                      # Name of the CSV file that needs to be processed (likely created by benchmark.sh.jinja)
+csv_final = args.final                  # Name of the new CSV file that will be created, with completed lines
 
-  csv_tmp = args.tmp                      # Name of the CSV file that needs to be processed (likely created by benchmark.sh.jinja)
-  csv_final = args.final                  # Name of the new CSV file that will be created, with completed lines
-  keep = args.keep                        # Flag to not rename the treated CSV file
+# ===================================================================
+# ===================================================================
+#                       PREPARATION STEP
+# ===================================================================
+# ===================================================================
 
-  # ===================================================================
-  # ===================================================================
-  #                       PREPARATION STEP
-  # ===================================================================
-  # ===================================================================
+# Get the size of the terminal in order to have a prettier output, if you need something more robust, go check http://granitosaurus.rocks/getting-terminal-size.html
 
-  # Get the size of the terminal in order to have a prettier output, if you need something more robust, go check http://granitosaurus.rocks/getting-terminal-size.html
+columns, rows = shutil.get_terminal_size()
 
-  columns, rows = shutil.get_terminal_size()
+# Output Header
 
-  # Output Header
+print("".center(columns,"*"))
+print("")
+print("EXECUTION OF THE BENCHMARKING SCRIPT FOR CECI CLUSTERS JOBS BEGINS NOW".center(columns))
+print("")
+print("".center(columns,"*"))
 
-  print("".center(columns,"*"))
-  print("")
-  print("EXECUTION OF THE BENCHMARKING SCRIPT FOR CECI CLUSTERS JOBS BEGINS NOW".center(columns))
-  print("")
-  print("".center(columns,"*"))
+section_title = "0. Preparation step"
 
-  section_title = "0. Preparation step"
+print("")
+print("")
+print(''.center(len(section_title)+10, '*'))
+print(section_title.center(len(section_title)+10))
+print(''.center(len(section_title)+10, '*'))
 
-  print("")
-  print("")
-  print(''.center(len(section_title)+10, '*'))
-  print(section_title.center(len(section_title)+10))
-  print(''.center(len(section_title)+10, '*'))
+# =========================================================
+# Check csv_tmp argument
+# =========================================================
 
-  # =========================================================
-  # Check csv_tmp argument
-  # =========================================================
+if (not os.path.exists(csv_tmp) or not os.path.isfile(csv_tmp)):
+  print("Error - No such file: {}".format(csv_tmp))
+  print("Aborting...")
+  exit(1)
 
-  if (not os.path.exists(csv_tmp) or not os.path.isfile(csv_tmp)):
-    print("Error - No such file: {}".format(csv_tmp))
-    print("Aborting...")
-    exit(1)
+# =========================================================
+# Initialize some variables
+# =========================================================
 
-  # =========================================================
-  # Initialize some variables
-  # =========================================================
+tmp_list = []
+csv_tmp_header = ""
+final_list = []
+csv_final_header = ""
+errors_list = []
 
-  tmp_list = []
-  csv_tmp_header = ""
-  final_list = []
-  csv_final_header = ""
+# =========================================================
+# Load temporary CSV file
+# =========================================================
 
-  # =========================================================
-  # Load temporary CSV file
-  # =========================================================
+print("\nScanning tmp file {} ... ".format(csv_tmp))
+with open(csv_tmp, 'r', newline='') as inputfile:
+  csv_content = csv.DictReader(inputfile, delimiter=';')
+  tmp_list = list(csv_content)
+  csv_tmp_header = csv_content.fieldnames
+  dialect = csv_content.dialect
+  print("    Detected CSV dialect in tmp file: {}".format(dialect))
+  print("    Detected CSV header in tmp file : {}".format(csv_tmp_header))
 
-  print("\nScanning tmp file {} ... ".format(csv_tmp))
-  with open(csv_tmp, 'r', newline='') as inputfile:
-    csv_content = csv.DictReader(inputfile, delimiter=';')
-    tmp_list = list(csv_content)
-    csv_tmp_header = csv_content.fieldnames
-    dialect = csv_content.dialect
-    print("    Detected CSV dialect in tmp file: {}".format(dialect))
-    print("    Detected CSV header in tmp file : {}".format(csv_tmp_header))
+# ===================================================================
+# ===================================================================
+#                         GETTING THE VALUES
+# ===================================================================
+# ===================================================================
 
-  # ===================================================================
-  # ===================================================================
-  #                         GETTING THE VALUES
-  # ===================================================================
-  # ===================================================================
+section_title = "1. Get benchmarking values"
 
-  section_title = "1. Get benchmarking values"
+print("")
+print("")
+print(''.center(len(section_title)+10, '*'))
+print(section_title.center(len(section_title)+10))
+print(''.center(len(section_title)+10, '*'))
 
-  print("")
-  print("")
-  print(''.center(len(section_title)+10, '*'))
-  print(section_title.center(len(section_title)+10))
-  print(''.center(len(section_title)+10, '*'))
+print("\nProcessing lines ...")
 
-  print("\nProcessing lines ...")
+for line in tmp_list:
 
-  for line in tmp_list:
-    
-    # For more informations on try/except structures, see https://www.tutorialsteacher.com/python/exception-handling-in-python
-    try:
+  new_line = line.copy()
+  
+  # For more informations on try/except structures, see https://www.tutorialsteacher.com/python/exception-handling-in-python
+  try:
 
-      # Header
+    # Header
 
-      print("")
-      print(''.center(60, '-'))
-      mol_name = str(line['Mol Name'])
-      print("{:>20}: {:<}".format("Mol Name",mol_name))
+    print("")
+    print(''.center(60, '-'))
+    mol_name = str(line['Mol Name'])
+    print("{:>20}: {:<}".format("Mol Name",mol_name))
 
-      jobID = str(line['Job ID'])
-      if (jobID == ""):
-        print("No JobID found. Skipping line... \n")
-        continue
-      print("{:>20}: {:<}".format("Job ID",jobID))
-      
-      print(''.center(60, '-'))
-
-      # Time information
-
-      reserved = get_Reserved(jobID)
-      print("{:>20}: {:<}".format("Reserved",reserved))
-      line['Reserved'] = reserved
-
-      elapsed = get_Elapsed(jobID)
-      print("{:>20}: {:<}".format("Elapsed",elapsed))
-      line['Elapsed'] = elapsed
-
-      walltime = get_Timelimit(jobID)
-      print("{:>20}: {:<}".format("Walltime",walltime))
-
-      elapsed_raw = slurm_time_to_seconds(elapsed)
-      walltime_raw = slurm_time_to_seconds(walltime)
-      time_eff = round(elapsed_raw / walltime_raw, 4)
-      print("{:>20}: {:<}".format("Time Efficiency","{:.0%}".format(time_eff)))
-      line['Time Efficiency'] = time_eff
-
-      print(''.center(60, '-'))
-
-      # Memory information
-
-      maxRSS = get_MaxRSS(jobID)
-      print("{:>20}: {:<} MB".format("MaxRSS",maxRSS))
-      line['Max RSS (MB)'] = maxRSS
-
-      nb_cpus = get_ReqCPUs(jobID)
-      mem_per_cpu = get_ReqMem(jobID)
-      tot_mem = nb_cpus * mem_per_cpu
-      print("{:>20}: {:<} MB ({} MB for each of {} CPUs)".format("Total MEM",tot_mem,mem_per_cpu,nb_cpus))
-
-      mem_eff = round(maxRSS / tot_mem, 4)
-      print("{:>20}: {:<}".format("RAM Efficiency","{:.0%}".format(mem_eff)))
-      line['RAM Efficiency'] = mem_eff
-
-      print(''.center(60, '-'))
-
-      # CPU Information
-
-      totCPU = get_TotCPU(jobID)
-      print("{:>20}: {:<}".format("TotalCPU",totCPU))
-      line['Total CPU'] = totCPU
-
-      wallCPU = get_CPUTime(jobID)
-      print("{:>20}: {:<}".format("Wall CPU",wallCPU))
-      line['Wall CPU'] = wallCPU
-
-      totCPU_raw = slurm_time_to_seconds(totCPU)
-      wallCPU_raw = slurm_time_to_seconds(wallCPU)
-      cpu_eff = round(totCPU_raw / wallCPU_raw, 4)
-      print("{:>20}: {:<}".format("CPU Efficiency","{:.0%}".format(cpu_eff)))
-      line['CPU Efficiency'] = cpu_eff
-
-      print(''.center(60, '-'))
-      print("")
-
-      # Add informations to our final CSV
-
-      final_list.append(line)
-
-    except Exception as error:
-      print(error)
+    jobID = str(line['Job ID'])
+    if (jobID is ""):
+      print("No JobID found. Skipping line... \n")
       continue
+    print("{:>20}: {:<}".format("Job ID",jobID))
+    
+    print(''.center(60, '-'))
 
-  if final_list == []:
-    print("ERROR: None of the lines were processed correctly")
-    print("Aborting ...")
-    exit(1)
+    # Time information
 
-  print("\nEnd of processing")
+    reserved = get_Reserved(jobID)
+    print("{:>20}: {:<}".format("Reserved",reserved))
+    new_line['Reserved'] = reserved
 
-  # ===================================================================
-  # ===================================================================
-  #                            THE END STEP
-  # ===================================================================
-  # ===================================================================
+    elapsed = get_Elapsed(jobID)
+    print("{:>20}: {:<}".format("Elapsed",elapsed))
+    new_line['Elapsed'] = elapsed
 
-  section_title = "2. Writing new information to final CSV file"
+    walltime = get_Timelimit(jobID)
+    print("{:>20}: {:<}".format("Walltime",walltime))
 
-  print("")
-  print("")
-  print(''.center(len(section_title)+10, '*'))
-  print(section_title.center(len(section_title)+10))
-  print(''.center(len(section_title)+10, '*'))
-  print("")
+    elapsed_raw = slurm_time_to_seconds(elapsed)
+    walltime_raw = slurm_time_to_seconds(walltime)
+    time_eff = round(elapsed_raw / walltime_raw, 4)
+    print("{:>20}: {:<}".format("Time Efficiency","{:.0%}".format(time_eff)))
+    new_line['Time Efficiency'] = time_eff
 
+    print(''.center(60, '-'))
+
+    # Memory information
+
+    maxRSS = get_MaxRSS(jobID)
+    print("{:>20}: {:<} MB".format("MaxRSS",maxRSS))
+    new_line['Max RSS (MB)'] = maxRSS
+
+    nb_cpus = get_ReqCPUs(jobID)
+    mem_per_cpu = get_ReqMem(jobID)
+    tot_mem = nb_cpus * mem_per_cpu
+    print("{:>20}: {:<} MB ({} MB for each of {} CPUs)".format("Total MEM",tot_mem,mem_per_cpu,nb_cpus))
+
+    mem_eff = round(maxRSS / tot_mem, 4)
+    print("{:>20}: {:<}".format("RAM Efficiency","{:.0%}".format(mem_eff)))
+    new_line['RAM Efficiency'] = mem_eff
+
+    print(''.center(60, '-'))
+
+    # CPU Information
+
+    totCPU = get_TotCPU(jobID)
+    print("{:>20}: {:<}".format("TotalCPU",totCPU))
+    new_line['Total CPU'] = totCPU
+
+    wallCPU = get_CPUTime(jobID)
+    print("{:>20}: {:<}".format("Wall CPU",wallCPU))
+    new_line['Wall CPU'] = wallCPU
+
+    totCPU_raw = slurm_time_to_seconds(totCPU)
+    wallCPU_raw = slurm_time_to_seconds(wallCPU)
+    cpu_eff = round(totCPU_raw / wallCPU_raw, 4)
+    print("{:>20}: {:<}".format("CPU Efficiency","{:.0%}".format(cpu_eff)))
+    new_line['CPU Efficiency'] = cpu_eff
+
+    print(''.center(60, '-'))
+    print("")
+
+    # Add informations to our final CSV
+
+    final_list.append(new_line)
+
+  except Exception as error:
+    print(error)
+    errors_list.append(line)
+    continue
+
+print("\nEnd of processing")
+
+# ===================================================================
+# ===================================================================
+#                            THE END STEP
+# ===================================================================
+# ===================================================================
+
+# =========================================================
+# Add information to final CSV file
+# =========================================================
+
+section_title = "2. Writing new information to final CSV file"
+
+print("")
+print("")
+print(''.center(len(section_title)+10, '*'))
+print(section_title.center(len(section_title)+10))
+print(''.center(len(section_title)+10, '*'))
+print("")
+
+if final_list == []:
+  print("ERROR: None of the lines were processed correctly.")
+else:
   # Define the final CSV header
 
   csv_final_header = csv_tmp_header + ["Reserved", "Elapsed", "Time Efficiency", "Max RSS (MB)", "RAM Efficiency", "Total CPU", "Wall CPU", "CPU Efficiency"]
@@ -304,7 +310,7 @@ def main():
     with open(csv_final, 'r') as f:
       write_header = (not f.readline()) # If the file is empty, write_header = True. Otherwise, write_header = False
 
-  # Open the final benchmark file in 'Append' mode and add processed lines (+ write header if required)
+  # Open the final CSV file in 'Append' mode and add processed lines (+ write header if required)
 
   print("\nWriting newly processed lines to the final file {} ...".format(csv_final), end='')
 
@@ -317,13 +323,49 @@ def main():
 
   print("{:>12}".format("[DONE]"))
 
-  print("")
-  print("".center(columns,"*"))
-  print("")
-  print("END OF EXECUTION".center(columns))
-  print("")
-  print("".center(columns,"*"))
+# =========================================================
+# Add problematic lines to a separate CSV file
+# =========================================================
 
+if errors_list != []:
+  section_title = "2bis. Writing problematic lines to a separate CSV file"
 
-if __name__ == "__main__":
-  main()
+  print("")
+  print("")
+  print(''.center(len(section_title)+10, '*'))
+  print(section_title.center(len(section_title)+10))
+  print(''.center(len(section_title)+10, '*'))
+  print("")
+
+  # Define the separate CSV header
+
+  print("Used dialect in the separate CSV file: {}".format(dialect))
+  print("Header used in the separate CSV file: {}".format(csv_tmp_header))
+
+  # Define if we have to write the header or not (only write it if the file does not exist or is empty)
+
+  csv_prob = os.path.splitext(csv_final)[0] + "_prob" + os.path.splitext(csv_final)[1]
+  write_header = True
+  if (os.path.exists(csv_prob) and os.path.isfile(csv_prob)):
+    with open(csv_prob, 'r') as f:
+      write_header = (not f.readline()) # If the file is empty, write_header = True. Otherwise, write_header = False
+
+  # Open the separate CSV file in 'Append' mode and add problematic lines (+ write header if required)
+
+  print("\nWriting problematic lines to a separate CSV file {} ...".format(csv_prob), end='')
+
+  with open(csv_prob, 'a', newline='') as prob_f:
+    csv_writer = csv.DictWriter(prob_f, fieldnames=csv_tmp_header, delimiter=';', quoting=csv.QUOTE_MINIMAL)
+    if write_header:
+      csv_writer.writeheader()
+    for line in errors_list:
+      csv_writer.writerow(line)
+
+  print("{:>12}".format("[DONE]"))
+
+print("")
+print("".center(columns,"*"))
+print("")
+print("END OF EXECUTION".center(columns))
+print("")
+print("".center(columns,"*"))
