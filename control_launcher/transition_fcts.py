1af71c6c17b4f34138d27799def6a5d3bcb52fa5
niacobel <56349930+niacobel@users.noreply.github.com>
2021-05-26 14:48:07 +0200

    Created the new brightests_to_darkests transition function (needs testing)

--- control_launcher/transition_fcts.py
+++ control_launcher/transition_fcts.py
@@ -8,7 +8,7 @@
 import contextlib
 import os
 
-import numpy
+import numpy as np
 
 import control_errors
 
@@ -47,29 +47,21 @@ def build_transition(init_state:int,target_state:int,init_label:str,target_label
           - ``momdip_key`` is the key of the transition dipole moments matrix used for this transition.
     """
 
-    # Defining the projector for the initial state
-
-    init_proj = numpy.zeros((len(system['eigenstates_list']), len(system['eigenstates_list'])),dtype=complex)  # Quick init of a zero-filled matrix
-    
-    init_proj[init_state][init_state] = 1 + 0j
-
-    # Creating the initial population file content by converting the projector to the eigenstates basis set
+    # Defining the initial population file name and content
 
     init_file = init_label + "_"
 
-    init_content = numpy.matmul(numpy.matmul(system['transpose'],init_proj),system['eigenvectors'])
+    init_content = np.zeros((len(system['eigenstates_list']), len(system['eigenstates_list'])),dtype=complex)  # Quick init of a zero-filled matrix
 
-    # Defining the projector for the target state
+    init_content[init_state][init_state] = 1 + 0j
 
-    target_proj = numpy.zeros((len(system['eigenstates_list']), len(system['eigenstates_list'])),dtype=complex)  # Quick init of a zero-filled matrix
-    
-    target_proj[target_state][target_state] = 1 + 0j
-
-    # Creating the target population file content by converting the projector to the eigenstates basis set
+    # Defining the target population file name and content
     
     target_file = target_label + "_"
 
-    target_content = numpy.matmul(numpy.matmul(system['transpose'],target_proj),system['eigenvectors'])
+    target_content = np.zeros((len(system['eigenstates_list']), len(system['eigenstates_list'])),dtype=complex)  # Quick init of a zero-filled matrix
+
+    target_content[target_state][target_state] = 1 + 0j
 
     # Calculate the transition energy (in Ha)
 
@@ -96,6 +88,122 @@ def build_transition(init_state:int,target_state:int,init_label:str,target_label
 #                        Transition functions                         #
 # =================================================================== #
 # =================================================================== #
+
+def brightests_to_darkests(system:dict):
+    """Determines the transition files needed by QOCT-RA for the transition between each of the first B brightest states and each of the first D darkest states. The brightest states are identified by their transition dipole moment with the ground state while the darkest states are identified by their radiative lifetime. B and D are fixed parameters that can easily be changed at the beginning of the function definition.
+
+    Parameters
+    ----------
+    system : dict
+        Information extracted by the parsing function and derived from it.
+
+    Returns
+    -------
+    transitions_list : list
+        List of dictionaries containing nine keys each: 
+
+          - ``label`` is the label of the transition, which will be used for the name of the job directories.
+          - ``init_state`` is the number of the initial state.
+          - ``target_state`` is the number of the target state.
+          - ``energy`` is the transition energy between the two states.
+          - ``init_file`` is the name of the initial state file, minus the number at the end.
+          - ``init_content`` is the content of the initial state file.
+          - ``target_file`` is the name of the target state file, minus the number at the end.
+          - ``target_content`` is the content of the target state file.
+          - ``momdip_key`` is the key of the transition dipole moments matrix used for this transition.
+    """  
+
+    # Initialize the dictionary that will be returned by the function
+
+    transitions_list = []
+
+    # Set the number of bright and dark states to consider
+    #! Keep in mind that you will end up with a total number of transitions corresponding to bmax * dmax * the number of transition dipole moments matrices!
+
+    bmax = 2 # Maximum number of bright states to consider (e.g. if bmax = 2, then only the two brightest states will be considered)
+    dmax = 2 # Maximum number of dark states to consider (e.g. if dmax = 2, then only the two darkest states will be considered)
+
+    # ========================================================= #
+    #                   Defining transitions                    #
+    # ========================================================= #
+
+    # We need to consider each transition dipole moments matrix separately
+    for momdip_key in system['momdip_es_mtx']:
+
+      # ========================================================= #
+      #          Defining the bright-dark pair of states          #
+      # ========================================================= #
+
+      # Sorting the bright states
+      # =========================
+
+      # Consider the dipole moments for transitions involving the ground state (and make sure it's a list and not a NumPy array)
+      gs_line = system['momdip_es_mtx'][momdip_key][0]
+      if isinstance(gs_line, np.ndarray):
+        gs_line = gs_line.tolist()
+
+      # Sort the indices of the states by decreasing order of the absolute value of their transition dipole moments
+      # e.g. if gs_line = [0.0, 0.0, 0.0, 0.0, 0.0, -1.2, 0.8, 1.0, -0.4] then bright_max_indices = [5, 7, 6, 8]
+      abs_gs_line = [abs(mom) for mom in gs_line]
+      bright_max_indices = [abs_gs_line.index(mom) for mom in sorted(abs_gs_line, reverse=True)]
+
+      # Sorting the dark states
+      # =======================
+
+      # Sort the indices of the states by decreasing order of their radiative lifetime
+      dark_max_indices = [system['eigenstates_list'].index(eigenstate) for eigenstate in sorted(system['eigenstates_list'], key = lambda i: i['lifetime'], reverse=True)]
+
+      # Iterate over the bright states
+      # ==============================
+
+      # Start iterating over the first Nth brightest states, where N is the bmax argument (see https://stackoverflow.com/questions/36106712/how-can-i-limit-iterations-of-a-loop-in-python for details)
+      for iter_bright, bright_index in zip(range(bmax), bright_max_indices):
+
+      # iter_bright is the number of the current iteration of this loop, e.g if bmax = 3, then iter_bright will be 0, then 1, then 2. (useful to label the transition)
+      # bright_index is the number of the state currently considered, e.g. if bright_max_indices = [5, 7, 6, 8] and iter_bright = 1, then bright_index = 7.
+
+        # Iterate over the dark states
+        # ============================
+
+        # Start iterating over the first Nth darkest states, where N is the dmax argument (see https://stackoverflow.com/questions/36106712/how-can-i-limit-iterations-of-a-loop-in-python for details)
+        for iter_dark, dark_index in zip(range(dmax), dark_max_indices):
+
+        # iter_dark is the number of the current iteration of this loop, e.g if dmax = 2, then iter_dark will be 0, then 1. (useful to label the transition)
+        # dark_index is the number of the state currently considered, e.g. if dark_max_indices = [4, 3, 2, 1] and iter_bright = 0, then dark_index = 4.
+
+          # ========================================================= #
+          #            Building the transition dictionary             #
+          # ========================================================= #
+
+          bright_label = system['eigenstates_list'][bright_index]["label"]
+          dark_label = system['eigenstates_list'][dark_index]["label"]
+
+          # Call the build_transition function using the current information for this transition
+
+          transition = build_transition(bright_index,dark_index,bright_label,dark_label,momdip_key,system)
+
+          # Update the label to something less generic and more informative
+          # e.g. X_1B2D_E8-E2 indicates a transition based on the transition dipole moments matrix associated with the "X" key (X axis), involving the brightest state (1B) and the second darkest state (2D), the labels of those states being E8 and E2, respectively.
+
+          transition_label = momdip_key + "_" + str(iter_bright+1) + "B" + str(iter_dark+1) + "D_" + bright_label + "-" + dark_label 
+          transition.update({ "label": transition_label}) 
+
+          # Pretty recap for the log file
+
+          print("")
+          print(''.center(50, '-'))
+          print("{:<20} {:<30}".format("Label: ", transition["label"]))
+          print(''.center(50, '-'))
+          print("{:<20} {:<30}".format("Initial state: ", "%s (%s)" % (bright_label,bright_index)))
+          print("{:<20} {:<30}".format("Target state: ", "%s (%s)" % (dark_label,dark_index)))
+          print("{:<20} {:<30}".format("Energy (Ha): ", "{:.4e}".format(transition["energy"])))
+          print(''.center(50, '-'))
+
+          # Add the transition to the transitions list, before proceeding with the next darkest state (or the next brightest state, if this was the last one)
+
+          transitions_list.append(transition)
+
+    return transitions_list
 
 def closest_bright_to_dark(system:dict):
     """Determines the transition files needed by QOCT-RA for the transition between the bright and dark states with the lowest transition energy between themselves, in the eigenstates basis set.
@@ -246,7 +354,7 @@ def brightests_to_coupled_darks(system:dict):
 
       # Consider the dipole moments for transitions involving the ground state (and make sure it's a list and not a NumPy array)
       gs_line = system['momdip_es_mtx'][momdip_key][0]
-      if isinstance(gs_line, numpy.ndarray):
+      if isinstance(gs_line, np.ndarray):
         gs_line = gs_line.tolist()
 
       # Sort the number of the bright states by decreasing order of the absolute value of their transition dipole moments
@@ -268,7 +376,7 @@ def brightests_to_coupled_darks(system:dict):
 
         # Consider the dipole moments for transitions involving the current bright state (and make sure it's a list and not a NumPy array)
         bright_line = system['momdip_es_mtx'][momdip_key][bright_number]
-        if isinstance(bright_line, numpy.ndarray):
+        if isinstance(bright_line, np.ndarray):
           bright_line = bright_line.tolist()
 
         # Sort the number of the dark states by decreasing order of the absolute value of their transition dipole moments with the current bright state
