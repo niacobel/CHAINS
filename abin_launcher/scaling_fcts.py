87e2f9bd3e1d27ad1f3e257c0a69540eac3b75db
niacobel <56349930+niacobel@users.noreply.github.com>
2022-07-27 15:58:09 +0200

    Added scaling function for number of valence electrons

--- abin_launcher/scaling_fcts.py
+++ abin_launcher/scaling_fcts.py
@@ -6,6 +6,7 @@
 ################################################################################################################################################
 
 import abin_errors
+import re
 
 def total_nb_elec(mendeleev:dict,file_data:dict):
     """Calculates the total number of electrons in a molecule.
@@ -85,6 +86,10 @@ def total_nb_elec(mendeleev:dict,file_data:dict):
 
     return total_elec
 
+
+######################################################################################################################################
+
+
 def total_nb_atoms(mendeleev:dict,file_data:dict):
     """Returns the total number of atoms in a molecule.
 
@@ -110,3 +115,94 @@ def total_nb_atoms(mendeleev:dict,file_data:dict):
     print("Total number of atoms in the molecule: ",total_atoms)
 
     return total_atoms
+
+
+######################################################################################################################################
+
+
+def valence_nb_elec(mendeleev:dict,file_data:dict):
+    """Calculates the total number of valence electrons in a molecule.
+
+    Parameters
+    ----------
+    mendeleev : dict
+        Content of AlexGustafsson's Mendeleev Table YAML file, which can be found at https://github.com/AlexGustafsson/molecular-data.
+    file_data : dict
+        The extracted information of the geometry file.
+
+    Returns
+    -------
+    total_val_elec : int
+        Total number of valence electrons in the molecule.
+
+    Raises
+    ------
+    AbinError
+        If there is no atomic number defined in mendeleev for one of the constituting atoms of the molecule. *(This exception is raised in the* ``get_nb_elec_for_element`` *subfunction.)*
+    """
+
+    # Definition of the function that can fetch the number of electrons associated with each element in Gustafsson's table
+
+    def get_val_elec_for_element(symbol:str, mendeleev:dict):
+        """Returns the number of valence electrons for a specific element.
+
+        Parameters
+        ----------
+        symbol : str
+            The atom symbol of the element.
+        mendeleev : dict
+            Content of AlexGustafsson's Mendeleev Table YAML file, which can be found at https://github.com/AlexGustafsson/molecular-data.
+
+        Returns
+        -------
+        nb_val_elec : int
+           Number of valence electrons.
+        
+        Raises
+        ------
+        AbinError
+            If there is no atomic number defined in mendeleev for one of the constituting atoms of the molecule.
+        """
+
+        nb_val_elec = 0
+
+        # Scan the mendeleev table and get the electronic configuration of our atom
+
+        for element in mendeleev:
+          if (element['symbol'] == symbol):
+            el_con = element['electronConfiguration']
+            if type(el_con) == int:
+              # For H and He, the number of valence electrons is directly given
+              nb_val_elec = el_con
+            else:
+              # For other atoms, we need to extract that number from the electronic configuration given in the form "[Aa] XyZ MnO"
+              split_el_con = re.split(r'\.|\s+',el_con) # Split the electronic configuration with either point or space delimiters
+              del split_el_con[0]                       # Remove the [ ] part of the electron configuration
+              for term in split_el_con:
+                nb_val_elec += int(term[2:])
+        
+        if nb_val_elec == 0:
+          raise abin_errors.AbinError ("ERROR: There is no electronic configuration defined for %s in AlexGustafsson's Mendeleev Table YAML file (mendeleev.yml)" % symbol)
+
+        return nb_val_elec
+
+    # Calculating the total number of electrons in the molecule
+
+    total_val_elec = 0
+
+    print("")
+    print(''.center(69, '-'))
+    print("{:<12} {:<10} {:<18} {:<25}".format('Atom Type','Valence','Number of atoms','Nb of valence electrons'))
+    print(''.center(69, '-'))
+
+    for atom,nb_atom in file_data['chemical_formula'].items():
+      val_elec = get_val_elec_for_element(atom,mendeleev)
+      subtotal_val_elec = nb_atom * val_elec
+      print("{:<12} {:<10} {:<18} {:<25}".format(atom, val_elec, nb_atom, subtotal_val_elec))
+      total_val_elec += subtotal_val_elec
+
+    print(''.center(69, '-'))
+    print("{:<23} {:<18} {:<25}".format('Total',sum(file_data['chemical_formula'].values()),total_val_elec))
+    print(''.center(69, '-'))
+
+    return total_val_elec
